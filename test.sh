53c43cf9f5a3402e06fa5bd94cfbf13b278578f5
apollo994 <fabio.zanarello.94@gmail.com>
2025-01-21 12:00:18 +0100

    slurm config

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-nextflow run main.nf \
+nextflow run -profile slurm main.nf \
     --gff_folder test/input \
     --outputFolder test/output \
     --ref $(realpath test/ref/Arabidopsis_lyrata.v.1.0.dna.chromosome.8.fa) \
